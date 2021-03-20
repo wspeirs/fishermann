904a4e24c96a931c2f5d873e1740117a20d0cfbe
William Speirs <bill.speirs@gmail.com>
2021-03-20 10:14:16 -0400

    Added results for LLS

--- scripts/train.py
+++ scripts/train.py
@@ -5,17 +5,21 @@ x_data = []
 y_data = []
 
 # read in the data file
-with open('../data/rand_gen.values', 'r') as fp:
+with open('../data/rand_gen_1m.values', 'r') as fp:
     for line in fp.readlines():
         score, values = line.strip().split(':')
 
         y_data.append(float(score))  # convert to a float
         x_data.append([float(x) for x in values.split(' ')])  # convert to an array of floats
 
+print("DONE READING DATA")
 
-# convert to numpy array
-y_data = np.array(y_data)
-x_data = np.array(x_data)
+# break up into training and testing data
+train_len = int(len(y_data) * 0.90)  # use 90% of the data for training
+y_train_data = np.array(y_data[:train_len])
+x_train_data = np.array(x_data[:train_len])
+y_test_data = np.array(y_data[train_len:])
+x_test_data = np.array(x_data[train_len:])
 
 # this _should_ always be 65, but get it programmatically
 x_data_width = len(x_data[0])
@@ -26,22 +30,28 @@ model = tf.keras.models.Sequential()
 # start with an input layer with a shape the same size as a single array, by as many arrays as we have (left blank)
 model.add(tf.keras.layers.InputLayer(input_shape=(x_data_width,)))
 
-# add a dense layer after our input that's twice the size
-model.add(tf.keras.layers.Dense(x_data_width*2, activation='relu'))
+# go through and add dense layers starting at x_data_width^2 until we get to 1
+cur_nodes = x_data_width**2
+while True:
+    # add a dense layer after our input that's twice the size
+    model.add(tf.keras.layers.Dense(cur_nodes, activation='relu'))
 
-# add another dense layer that's the same size
-model.add(tf.keras.layers.Dense(x_data_width, activation='relu'))
+    cur_nodes = int(cur_nodes / 4)
 
-# another dense layer that's floor of half the size
-model.add(tf.keras.layers.Dense(int(x_data_width/2), activation='relu'))
-
-# finally a single-output layer
-model.add(tf.keras.layers.Dense(1))
+    if cur_nodes <= 1:
+        model.add(tf.keras.layers.Dense(cur_nodes, activation='relu'))
+        break
 
 # create a loss function
-loss_fn = tf.keras.losses.BinaryCrossentropy()
+loss_fn = tf.keras.losses.LogCosh()
 
-model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+# compile the model with the loss function
+model.compile(optimizer=tf.keras.optimizers.SGD(0.8), loss=loss_fn, metrics=['accuracy'])
 
-model.fit(x_data, y_data, epochs=5)
+# fit our training data to the model
+model.fit(x_train_data, y_train_data, validation_data=(x_test_data, y_test_data), shuffle=True, epochs=5)
+
+# evaluate how well we did with test data
+model.evaluate(x_test_data, y_test_data, verbose=1)
+
 
--- utils/linear/src/main.rs
+++ utils/linear/src/main.rs
@@ -1,15 +1,56 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
-use nalgebra::{VectorN, DMatrix, MatrixN, U65};
+use nalgebra::{DVector, VectorN, DMatrix, MatrixN, U65};
+use nalgebra::linalg::QR;
+use std::collections::BTreeMap;
+
+
+const NUM_ROWS :usize = 975_774;
+const SCALE_AMT :f64 = 100_f64;
+
+#[inline]
+fn scale(x :f64) -> f64 {
+    if x == 0f64 {
+        x
+    } else if x < 0f64 {
+        x - SCALE_AMT
+    } else {
+        x + SCALE_AMT
+    }
+}
+
+#[inline]
+fn unscale(x :f64) -> f64 {
+    if x == 0f64 {
+        x
+    } else if x < 0f64 {
+        x + SCALE_AMT
+    } else {
+        x - SCALE_AMT
+    }
+}
+
+// #[inline]
+// fn unscale(x :f64) -> f64 {
+//     if x < SCALE_AMT {
+//         x + SCALE_AMT
+//     } else if x > SCALE_AMT {
+//         x - SCALE_AMT
+//     } else {
+//         x
+//     }
+// }
 
 fn main() {
     let mut file = BufReader::new(File::open("/home/wspeirs/src/fishermann/data/rand_gen_1m.values").unwrap());
 
-    let mut scores = Vec::new();
-    let mut values = Vec::new();
+    let mut scores = Vec::with_capacity(NUM_ROWS);
+    let mut values = Vec::with_capacity(NUM_ROWS * 65);
 
-    // read in the first 65 lines
-    for _ in 0..65 {
+    println!("SCALE: {}", SCALE_AMT);
+
+    // read in the first NUM_ROWS lines
+    for _ in 0..NUM_ROWS {
         let mut line = String::new();
         file.read_line(&mut line);
 
@@ -18,20 +59,71 @@ fn main() {
         let fields = line.split(":").collect::<Vec<_>>();
 
         // add on the score
-        scores.push(fields[0].parse::<f64>().unwrap());
+        scores.push(scale(fields[0].parse::<f64>().unwrap()));
 
-        values.extend(fields[1].split(" ").filter_map(|s| if s.is_empty() { None } else { Some(s.parse::<f64>().unwrap()) } ));
+        values.extend(fields[1].split(" ").filter_map(|s| if s.is_empty() { None } else { Some( scale(s.parse::<f64>().unwrap()) ) } ));
     }
 
-    println!("{} {}", scores.len(), values.len());
+    println!("READ VALUES");
+    // println!("{} {}", scores.len(), values.len());
 
-    let values = MatrixN::<f64, U65>::from_iterator(values.into_iter());
-    // let values = DMatrix::from_iterator(65, 65, values.into_iter());
-    let scores = VectorN::<f64, U65>::from_iterator(scores.into_iter());
+    // let values = MatrixN::<f64, U65>::from_iterator(values.into_iter());
+    // let values = DMatrix::from_iterator(NUM_ROWS, 65, values.into_iter());
+    let values = DMatrix::from_vec(NUM_ROWS, 65, values);
+    // let scores = VectorN::<f64, U65>::from_iterator(scores.into_iter());
+    let scores = DVector::from_vec(scores);
 
-    let coefficients = values.qr().solve(&scores).unwrap();
+    // println!("SCORES: {}", scores);
+    // println!("VALUES: {}", values);
 
-    println!("SCORES: {}", scores);
-    println!("VALUES: {}", values);
-    println!("COEFF: {}", coefficients);
+    let qr_decomp = values.clone().qr();
+
+    let lls = qr_decomp.r().qr().try_inverse().unwrap() * qr_decomp.q().transpose() * scores.clone();
+
+    println!("COMPUTED LLS");
+
+    // println!("LLS: {}", lls);
+
+    // go through each one, and compute how far off we are
+    let computed_scores = values * lls;
+    let mut diffs = Vec::with_capacity(NUM_ROWS);
+
+    println!("COMPUTED SCORES");
+
+    for (real_score, computed_score) in scores.iter().zip(computed_scores.iter().map(|s| unscale(*s))) {
+        let diff = (real_score - computed_score).abs();
+        // println!("{} {} {}", real_score, computed_score, diff);
+        diffs.push(diff.floor() as u64);
+    }
+
+    let min = *diffs.iter().min().unwrap();
+    let max = *diffs.iter().max().unwrap();
+    let avg = diffs.iter().sum::<u64>() as f64 / NUM_ROWS as f64;
+
+    diffs.sort_unstable();
+
+    let p50 = diffs[(diffs.len() as f64 * 0.5f64) as usize];
+    let p75 = diffs[(diffs.len() as f64 * 0.75f64) as usize];
+    let p90 = diffs[(diffs.len() as f64 * 0.9f64) as usize];
+    let p95 = diffs[(diffs.len() as f64 * 0.95f64) as usize];
+
+    // create a histogram rounded to the nearest 100
+    let mut histo = BTreeMap::<u64, u64>::new();
+
+    for diff in diffs {
+        let rounded_diff = (diff / 100) * 100;
+
+        *histo.entry(rounded_diff).or_default() += 1;
+    }
+
+    println!();
+
+    // let mut sorted_keys = histo.keys().collect::<Vec<_>>();
+    // sorted_keys.sort_unstable();
+    //
+    // for key in sorted_keys {
+    //     println!("{}: {}", key, histo[key]);
+    // }
+
+    println!("MIN: {} AVG: {} MAX: {} P50: {} P75: {} P90: {} P95: {}", min, avg, max, p50, p75, p90, p95);
 }
